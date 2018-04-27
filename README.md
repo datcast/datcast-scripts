d60f59e82494b0cc22919e6b00ca700003fd8ce7
Jim Pick <jim@backblend.com>
2018-04-26 18:50:06 -0700

    First attempt

---
+++ README.md
@@ -0,0 +1,5 @@
+# datcast scripts
+
+Simple script to pull data from tryca.st and transmogrify so
+it can be copied into the dat archive.
+
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ mkdirp.sync('audio')
 
 console.log('Downloading feed')
 fetch('http://datcast.cast.rocks/feed.xml')
-  .then(response => response.text())
+  .then(res => res.text())
   .then(text => {
     text = text
       .replace(
@@ -23,11 +23,54 @@ fetch('http://datcast.cast.rocks/feed.xml')
         }
       )
     fs.writeFileSync('feed.xml', text)
-    console.log('Done.')
-
-    console.log('Downloading mp3s')
   })
   .then(() => {
+    console.log('Downloading HTML')
+    return fetch('http://datcast.cast.rocks/')
+  })
+  .then(res => res.text())
+  .then(text => {
+    // Example: https://ca-ns-1.bulkstorage.ca/v1/AUTH_0d09c87549084f4ba4ad4a9e807d0d76/5887/feeds/DCUP0.html?temp_url_sig=9c0a641a6288d71ab1bb0b597010500236978369&temp_url_expires=1524780293&inline
+    const re = 'body.*src="(https:\/\/ca-ns-1.bulkstorage.ca\/\.*DCUP0.html.*inline)"'
+    const match = text.match(new RegExp(re, 'm'))
+    if (!match) throw new Error("Couldn't match body url")
+    const url = match[1]
+    return fetch(url)
+  })
+  .then(res => res.text())
+  .then(text => {
+    text = text
+      .replace(
+        /https:\/\/cast-cache\.s3\.amazonaws.com(\/castette\/.*\.png)/g,
+        (_, g1) => g1
+      )
+      .replace(
+        /(https:)?\/\/cast\.rocks\/hosting\/5887\/feeds\/(DCUP0\.jpg)/g,
+        (_, g1, g2) => `/img/${g2}`
+      )
+      .replace(
+        /(https:)?\/\/cast\.rocks\/hosting\/5887\/feeds\/(DCUP0\.html)/g,
+        (_, g1, g2) => `/${g2}`
+      )
+      .replace(
+        /\/\/cast\.rocks\/hosting\/5887\/(.*\.mp3)/g,
+        (_, g1) => `/audio/${g1}`
+      )
+      .replace(
+        /https:\/\/cast-cache\.s3\.amazonaws.com\/(.*\.svg)/g,
+        (_, g1) => `/castette/${g1}`
+      )
+      .replace(
+        /http:\/\/cast\.rocks\/hosting\/5887\/feeds\/DCUP0\.xml/g,
+        () => `/feed.xml`
+      )
+    text = text.split('\n')
+      .filter(line => !line.match(/script.*typekit/i))
+      .join('\n')
+    fs.writeFileSync('DCUP0.html', text)
+  })
+  .then(() => {
+    console.log('Downloading mp3s')
     return mp3s.reduce((prevPromises, mp3) => {
       const promise = prevPromises
         .then(() => {
